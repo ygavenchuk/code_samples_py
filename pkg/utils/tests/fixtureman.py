8853be38e1a2a30abb6e9d9ceaaf25da2a741cda
Yuriy Gavenchuk <y.gavenchuk@bvblogic.com>
2014-07-09 11:00:59 +0300

    FixtureManager can define external caller

--- pkg/utils/tests/fixtureman.py
+++ pkg/utils/tests/fixtureman.py
@@ -1,15 +1,35 @@
 # -*- coding: utf-8 -*-
 
+__author__ = 'y.gavenchuk'
 
-import json
-import os
+
+from os.path import join as path_join, abspath, dirname
+from json import load as json_load
+from copy import deepcopy
+from inspect import stack as inspect_stack
+
+
+__all__ = ['FixtureManager', ]
 
 
 class FixtureManager(object):
+    @staticmethod
+    def _get_caller_filename():
+        """
+        Find and return filename of outer caller (external script)
+
+        :return str:
+        """
+        for item in inspect_stack():
+            if item and __file__ not in item:
+                return item[1]
+
+        return __file__
+
     def __init__(self):
         self.fixture_data = {}
 
-    def load(self, fixture_data=None, fixture_file=None, file_ext='json', current_file=__file__):
+    def load(self, fixture_data=None, fixture_file=None, file_ext='json', current_file=None):
         """
             Load fixtures from iterable (list, dict, tuple, ...) instance (fixture_data) or file
 
@@ -27,7 +47,7 @@ class FixtureManager(object):
             if file_ext:
                 fixture_file += '.' + file_ext
             with open(self.get_fixture_path(fixture_file, current_file), 'r') as fp:
-                self.fixture_data = json.load(fp)
+                self.fixture_data = json_load(fp)
         else:
             raise ImportError('Undefined fixture data source')
 
@@ -36,15 +56,17 @@ class FixtureManager(object):
             Get fixture data by it's name.
 
             :param fixture_name: string - name of fixture, which data need to return
-            :return: dict - if fixture_name doesn't exists - will be returned an empty dict
+            :return: Iterable - if fixture_name doesn't exists - will be returned an empty dict
         """
-        return self.fixture_data[fixture_name] if fixture_name in self.fixture_data else dict
+        result = self.fixture_data[fixture_name] if fixture_name in self.fixture_data else dict
+
+        return deepcopy(result)
 
     def __getitem__(self, item):
         return self.get(item)
 
     @classmethod
-    def get_fixture_path(cls, fixture_file, current_file=__file__):
+    def get_fixture_path(cls, fixture_file, current_file=None):
         """
             Build and return absolute path to the fixture's file.
 
@@ -55,4 +77,8 @@ class FixtureManager(object):
 
             :return: string
         """
-        return os.path.join(os.path.abspath(os.path.dirname(current_file)), *('fixtures', fixture_file,))
+
+        if not current_file:
+            current_file = cls._get_caller_filename()
+
+        return path_join(abspath(dirname(current_file)), 'fixtures', fixture_file)
